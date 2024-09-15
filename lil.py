1a1102a175f80d55331db1a8c6ad2082f413e24f
ayli0101 <serikbollajla70@gmail.com>
2024-09-15 16:58:52 +0500

    first commit

---
+++ lil.py
@@ -0,0 +1,10 @@
+from tkinter import *
+ 
+root = Tk()     
+root.title("Приложение на Tkinter")
+root.geometry("300x250")
+ 
+label = Label(text="Hello METANIT.COM")
+label.pack()    
+ 
+root.mainloop()
